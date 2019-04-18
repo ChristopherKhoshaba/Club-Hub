29f98321de2e9632f788b3c2974a48e68318dbc2
ckhosh2 <ckhosh2@illinois.edu>
2019-04-18 17:42:35 -0500

    "Tap here!" button is now stylized, colored, and functional. New activity, "RegisterActivity" added with a non-functional registration screen.

--- app/src/main/java/com/example/rsoswipe/MainActivity.java
+++ app/src/main/java/com/example/rsoswipe/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.rsoswipe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText mTextUsername;
+    EditText mTextPassword;
+    Button mButtonLogin;
+    TextView mTextViewRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextUsername = (EditText) findViewById(R.id.editText_username);
+        mTextPassword = (EditText) findViewById(R.id.editText_password);
+        mButtonLogin = (Button) findViewById(R.id.button_login);
+        mTextViewRegister = (TextView) findViewById(R.id.textView_register);
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 }
