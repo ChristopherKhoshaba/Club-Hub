a0c75556bd7f2ec3fd6d41355c44b9d64f1710e9
ckhosh2 <ckhosh2@illinois.edu>
2019-04-18 19:55:53 -0500

    Registration screen "login" button now returns user to LoginActivity.

--- app/src/main/java/com/example/rsoswipe/RegisterActivity.java
+++ app/src/main/java/com/example/rsoswipe/RegisterActivity.java
@@ -1,13 +1,36 @@
 package com.example.rsoswipe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText mTextUsername;
+    EditText mTextPassword;
+    EditText mTextCnfPassword;
+    Button mButtonRegister;
+    TextView mTextViewLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mTextUsername = (EditText) findViewById(R.id.editText_username);
+        mTextPassword = (EditText) findViewById(R.id.editText_password);
+        mTextCnfPassword = (EditText) findViewById(R.id.editText_cnf_password);
+        mButtonRegister = (Button) findViewById(R.id.button_login);
+        mTextViewLogin = (TextView) findViewById(R.id.textView_register);
+        mTextViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent LoginIntent = new Intent(RegisterActivity.this,MainActivity.class);
+                startActivity(LoginIntent);
+            }
+        });
     }
 }
